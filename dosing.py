16b413825dbb9ea237f40a8bf9210db58c7da083
Drew Genuit <drewgenuit@Drews-MacBook-Pro.local>
2021-09-06 12:43:10 -0400

    Creating and modularizing the dosage function for class

--- dosing.py
+++ dosing.py
@@ -7,6 +7,12 @@
 
 
 def dose_amount():
+    patient_diagnosis = dose_input()
+    patient_weight = weight_input()
+    dosage(patient_weight, patient_diagnosis)
+
+
+def dose_input():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -15,6 +21,9 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def weight_input():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
@@ -24,6 +33,9 @@ def dose_amount():
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+def dosage(weight, diagnosis):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
@@ -31,6 +43,7 @@ def dose_amount():
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
+    
 
 
 if __name__ == '__main__':
