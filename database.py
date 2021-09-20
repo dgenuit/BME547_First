e25da63903032e2ee20781129f8a0fb63dea3b28
Drew Genuit <drewgenuit@Drews-MacBook-Pro.local>
2021-09-20 13:16:30 -0400

    modified database to be a dictionary and implemented classes

--- database.py
+++ database.py
@@ -1,5 +1,21 @@
+class Patient:
+
+    def __init__(self, name, id_no, age):
+        self.name = name
+        self.id_no = id_no
+        self.age = age
+        self.tests = []
+    
+    def __repr__(self):
+        return "{}: {}".format(self.id_no, self.name)
+    
+
+
+
 def create_database_entry(patient_name, id_no, age):
-    new_patient = [patient_name, id_no, age, []]
+    new_patient = Patient(patient_name, id_no, age)
+
+    #new_patient = {"name": patient_name, "id_no": id_no, "age": age, "tests": []}
     return new_patient
 
 def print_database(db):
@@ -7,42 +23,42 @@ def print_database(db):
     for patient, location in zip(db, locations):
         print("{} - {}". format(patient, location))
 
-def main():
-    db = []
-    x = create_database_entry("Ann Ables", 120, 30)
-    db.append(x)
-    x = create_database_entry("Bob Boyles", 24, 31)
-    db.append(x)
-    x = create_database_entry("Chris Chou", 33, 33)
-    db.append(x)
-    x = create_database_entry("David Dinkins", 14, 34)
-    db.append(x)
 
+def get_patient(db, id_no):
+    patient = db[id_no]
+    return patient
+
+
+
+def main():
+
+    db = {}
+    x = create_database_entry("Ann Ables", 120, 30)
+    db[x.id_no] = x
+    x = create_database_entry("Bob Boyles", 24, 31)
+    db[x.id_no] = x
+    x = create_database_entry("Chris Chou", 33, 33)
+    db[x.id_no] = x
+    x = create_database_entry("David Dinkins", 14, 34)
+    db[x.id_no] = x
+
+    print(db)
     patient_id_tested = 24
     test_done = ("HDL", 65)
     patient = get_patient(db, patient_id_tested)
-    patient[3].append(test_done)
+    patient.tests.append(test_done)
+    print(db[24].tests)
 
     print_database(db)
-
-
-   
-
+    return
  
     
 def age_check(db, age_limit):
     for patient in db:
-        if patient[2] >= age_limit:
-            print(patient[0])
+        if patient["age"] >= age_limit:
+            print(patient["name"])
 
-
-def get_patient(db, id_no):
-    for patient in db:
-        if patient[1] == id_no:
-            return patient
 
 
 if __name__ == "__main__":
-    db = main()
-
-    
+    main()
